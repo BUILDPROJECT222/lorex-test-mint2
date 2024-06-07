f6543a06f70b7a47aa12f2c5244ea984d8353ecd
kuyol-project <153427860+BUILDPROJECT222@users.noreply.github.com>
2024-06-07 16:59:51 +0800

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -2,7 +2,7 @@
 // label has to be the exact same as in your candy machine config
 export const mintText = [
   { label: "OGs", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "Public", mintText: "Mint Paying with Loreks", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "Public", mintText: "Mint Paying with LOREKS Token", buttonLabel: "Mint now!", header: "Public Mint" },
 ];
 
 //header image in the ui. replace with your own
